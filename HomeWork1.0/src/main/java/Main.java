fe624851b17fad419c080851d70778236a1ac91d
s1t3w4n <s1t3w4n@yandex.ru>
2018-11-13 12:36:10 +0300

    my obfuscate

--- HomeWork1.0/src/main/java/Main.java
+++ HomeWork1.0/src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        printSomething(setString());
         System.out.println("Hello, my first obfuscated jar file!");
+    }
+    private static List<String> setString(){
+        List<String> lists = new ArrayList();
+        lists.add("there");
+        lists.add("is");
+        lists.add("my");
+        lists.add("dependencies");
+
+        return lists;
+    }
+    private static void printSomething(List<String> list) {
+        for (String s:list) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 }
