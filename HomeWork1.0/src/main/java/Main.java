eb8c02aefb516936a00ee5d9eec9e5a79a1735ea
s1t3w4n <s1t3w4n@yandex.ru>
2018-11-18 15:26:21 +0300

    fix of dependencies

--- HomeWork1.0/src/main/java/Main.java
+++ HomeWork1.0/src/main/java/Main.java
@@ -2,7 +2,7 @@ import com.google.common.math.IntMath;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(IntMath.factorial(47));
+        System.out.println("Dependency for guava:" + (IntMath.factorial(47)));
         System.out.println("Hello, my first obfuscated jar file!");
     }
 }
