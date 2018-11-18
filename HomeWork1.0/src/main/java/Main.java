ebcadcd703acc97bc81a61e223e13de0ad3d3415
s1t3w4n <s1t3w4n@yandex.ru>
2018-11-18 15:05:13 +0300

    fix of dependencies

--- HomeWork1.0/src/main/java/Main.java
+++ HomeWork1.0/src/main/java/Main.java
@@ -1,24 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.math.IntMath;
 
 public class Main {
     public static void main(String[] args) {
-        printSomething(setString());
+        System.out.println(IntMath.factorial(47));
         System.out.println("Hello, my first obfuscated jar file!");
-    }
-    private static List<String> setString(){
-        List<String> lists = new ArrayList();
-        lists.add("there");
-        lists.add("is");
-        lists.add("my");
-        lists.add("dependencies");
-
-        return lists;
-    }
-    private static void printSomething(List<String> list) {
-        for (String s:list) {
-            System.out.print(s + " ");
-        }
-        System.out.println();
     }
 }
