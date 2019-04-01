293d16788dc992cf4ee25f116210da4cd7576a38
s1t3w4n <s1t3w4n@yandex.ru>
2019-04-01 19:16:26 +0300

    HW 5 gc beta 1st CR fixes

--- HW05-gc/src/main/java/MainHW05.java
+++ HW05-gc/src/main/java/MainHW05.java
@@ -3,7 +3,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MainHW05 {
@@ -13,21 +13,43 @@ public class MainHW05 {
     public static void main(String[] args) throws InterruptedException {
         List<GarbageCollectorMXBean> gcBean = ManagementFactory.getGarbageCollectorMXBeans();
         LOGGER.info("Starting the program...");
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new LinkedList<>();
+        long beginTime = System.currentTimeMillis();
+        long minute = System.currentTimeMillis();
+        boolean selector = false;
 
-        for (int j = 0; j < 5_000_000; j++) {
-            list.add(j);
-            //out of memory
-            /*if (j % 2 == 0) {
-                list.set(j, new Integer(j + 1));
-            }*/
+
+        for (int j = 0; j < Integer.MAX_VALUE; j++) {
+            if (j % 5_000_000 == 0) {
+                if (selector) {
+                    selector = false;
+                } else {
+                    selector = true;
+                }
+            }
+            if (selector) {
+                list.add(j);
+            } else {
+                list.remove(0);
+            }
+            if (System.currentTimeMillis() - minute > 60 * 1000) {
+                minute = System.currentTimeMillis();
+                LOGGER.info("Time of work:" + (minute - beginTime) / 1000 + " sec");
+                showInformation(gcBean);
+            }
+            if ((System.currentTimeMillis() - beginTime) >= 300 * 1_000) {
+                LOGGER.info("Time of work: 5 min");
+                break;
+            }
         }
+        LOGGER.info("Last results: ");
         showInformation(gcBean);
+        LOGGER.info("Ending the program...");
     }
 
     private static void showInformation(List<GarbageCollectorMXBean> gcBean) {
 
-        LOGGER.info("Garbage collector monitoring");
+        LOGGER.info("===============Garbage collector monitoring===============");
 
         for (GarbageCollectorMXBean manager : gcBean) {
             LOGGER.info("Memory manager: " + manager.getName());
