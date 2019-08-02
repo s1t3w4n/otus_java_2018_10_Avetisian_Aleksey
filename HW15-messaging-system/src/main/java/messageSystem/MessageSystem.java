440518cfcf8d8959b6d076880453c7b78b6f03e7
s1t3w4n <s1t3w4n@yandex.ru>
2019-08-02 12:53:24 +0300

    HW 15, fixed Thread fail after NPE exception 2

--- HW15-messaging-system/src/main/java/messageSystem/MessageSystem.java
+++ HW15-messaging-system/src/main/java/messageSystem/MessageSystem.java
@@ -47,7 +47,6 @@ public final class MessageSystem {
                             message.exec(entry.getValue());
                         } catch (Exception e) {
                             logger.log(Level.INFO, "Thread failed. Finishing: " + name);
-                            continue;
                         }
                     }
                 }
